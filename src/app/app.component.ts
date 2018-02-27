1e836fb928b32a6967beb4fbd28ae1f7c03289a9
Misha <tymish1@gamail.com>
2018-02-27 12:38:00 +0200

    added toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'ml-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pr';
+export class AppComponent implements OnInit {
+  isHome;
+
+  constructor(private router: Router) {
+    router.events.filter(event => event instanceof NavigationEnd).subscribe(rs => (this.isHome = rs['url']));
+  }
+
+  get homePage() {
+    return this.isHome === '/';
+  }
+
+  ngOnInit() {}
 }
