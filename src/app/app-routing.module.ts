5e23a527503e8abbfe5c931b107feccbf6e0f084
Misha <tymish1@gamail.com>
2018-02-27 11:12:37 +0200

    fix: routes children rout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
-import {NoPreloading, PreloadAllModules, RouterModule, Routes} from '@angular/router';
-import {environment} from '../environments/environment';
-import {LandingPageComponent} from './pages/landing-page/landing-page.component';
-import {HomePageComponent} from './pages/home-page/home-page.component';
-import {NewsPageComponent} from './pages/news-page/news-page.component';
-import {ContactPageComponent} from './pages/contact-page/contact-page.component';
-import {PhotoGalleryPageComponent} from './pages/photo-gallery-page/photo-gallery-page.component';
+import { NoPreloading, PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { environment } from '../environments/environment';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NewsPageComponent } from './pages/news-page/news-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { PhotoGalleryPageComponent } from './pages/photo-gallery-page/photo-gallery-page.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: LandingPageComponent, data: { title: 'Главная' } },
   { path: 'home', component: HomePageComponent, data: { title: 'Головна' } },
   { path: 'news', component: NewsPageComponent, data: { title: 'Новини' } },
-  { path: 'news', component: ContactPageComponent, data: { title: 'Контакти' } },
-  { path: 'news', component: PhotoGalleryPageComponent, data: { title: 'Фотогалерея' } },
+  { path: 'contact', component: ContactPageComponent, data: { title: 'Контакти' } },
+  { path: 'photo', component: PhotoGalleryPageComponent, data: { title: 'Фотогалерея' } },
+  { path: 'tests', loadChildren: 'app/psyhology-test/psyhology-test.module#PsyhologyTestModule' },
   { path: '**', redirectTo: '/' }
 ];
 
@@ -22,4 +23,4 @@ export const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
