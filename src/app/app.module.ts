36b3b9d7c3129e99c2f9e4be1106cb56120ecee8
Misha <tymish1@gamail.com>
2018-02-27 12:37:43 +0200

    install @ngx-loading-bar package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+import { LoadingBarHttpModule } from '@ngx-loading-bar/http';
+import { LoadingBarModule } from '@ngx-loading-bar/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, PagesModule, SharedModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    PagesModule,
+    SharedModule,
+    LoadingBarHttpClientModule,
+    LoadingBarRouterModule,
+    LoadingBarHttpModule,
+    LoadingBarModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
