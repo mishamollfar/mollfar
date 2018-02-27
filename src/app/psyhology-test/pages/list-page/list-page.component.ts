68b0f4bdcb855fcbaf3e91226a3842a156f3b9d0
Misha <tymish1@gamail.com>
2018-02-27 12:56:57 +0200

    added template for lists tests

--- src/app/psyhology-test/pages/list-page/list-page.component.ts
+++ src/app/psyhology-test/pages/list-page/list-page.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-page.component.scss']
 })
 export class ListPageComponent implements OnInit {
+  tests = [
+    { name: 'тест на визначення бізнес-жилки', routerlink: '/tests/business' },
+    { name: 'тест на визначення організаторських здібностей', routerlink: '/tests/organizational' },
+    { name: 'тест на визначення рівня котактоності', routerlink: '/tests/contact-level' },
+    { name: 'тест на визначення рівня тривожності', routerlink: '/tests/anxiety-level' },
+    { name: 'тест на визначення самооцінки', routerlink: '/tests/self-esteem' }
+  ];
+
   constructor() {}
 
   ngOnInit() {}
