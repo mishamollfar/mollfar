be43ed427614e8f9ce889691d8747166225347de
Misha <tymish1@gamail.com>
2018-02-27 19:10:55 +0200

    create file question and file answer and added form to test page component

--- src/app/psyhology-test/pages/business-test-page/business-test-page.component.ts
+++ src/app/psyhology-test/pages/business-test-page/business-test-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { testBusiness } from './questions';
+import { answerTest } from './answer-test';
+import { PopupDialogsService } from '../../../shared/popup-dialogs/popup-dialogs.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ml-business-test-page',
@@ -6,7 +11,97 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./business-test-page.component.scss']
 })
 export class BusinessTestPageComponent implements OnInit {
-  constructor() {}
+  questions;
+  answers = ['a', 'b', 'c'];
+  questionsControl;
+  equalTest = false;
+  businessTest: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+    private fb: FormBuilder,
+    private info: PopupDialogsService,
+    private cd: ChangeDetectorRef,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.initForm();
+    this.questions = testBusiness;
+    this.questionsControl = Object.keys(this.questions);
+  }
+
+  initForm() {
+    this.businessTest = this.fb.group({
+      question1: this.fb.control('', [Validators.required]),
+      question2: this.fb.control('', [Validators.required]),
+      question3: this.fb.control('', [Validators.required]),
+      question4: this.fb.control('', [Validators.required]),
+      question5: this.fb.control('', [Validators.required]),
+      question6: this.fb.control('', [Validators.required]),
+      question7: this.fb.control('', [Validators.required])
+    });
+  }
+
+  clearForm() {
+    this.businessTest.reset();
+  }
+
+  equalResponse() {
+    this.equalTest = true;
+    const dialogMsg = this.getAnswer();
+    this.info
+      .open(dialogMsg, 'info')
+      .afterClosed()
+      .finally(() => {
+        this.equalTest = false;
+        this.cd.detectChanges();
+      })
+      .subscribe(rs => {
+        if (!rs) {
+          this.clearForm();
+        }
+        this.router.navigateByUrl(rs ? '/tests/list' : '/tests/business');
+      });
+  }
+
+  getCountAnswer() {
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    const responseArray = Object.keys(this.businessTest.value).map(item => this.businessTest.value[item]);
+
+    responseArray.forEach(key => {
+      if (key === 'a') {
+        a++;
+      } else if (key === 'b') {
+        b++;
+      } else {
+        c++;
+      }
+    });
+
+    return { a: a, b: b, c: c };
+  }
+
+  getAnswer() {
+    const counts = this.getCountAnswer();
+    let message;
+    const answer = Math.max(counts.a, counts.b, counts.c);
+
+    if (answer === counts.a) {
+      message = answerTest.a;
+    } else if (answer === counts.b) {
+      message = answerTest.b;
+    } else if (answer === counts.c) {
+      message = answerTest.c;
+    } else if (answer === counts.a && answer === counts.c) {
+      message = answerTest.ac;
+    } else if (answer === counts.a && answer === counts.b) {
+      message = answerTest.ab;
+    } else {
+      message = answerTest.bc;
+    }
+
+    return message;
+  }
 }
