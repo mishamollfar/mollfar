1c37e9a3bcfa4ee6657695fad2d7fb345b4c4489
Misha <tymish1@gamail.com>
2018-03-01 16:58:32 +0200

    added template and logic to selt-esteem-page.component

--- src/app/psyhology-test/pages/self-esteem-test-page/self-esteem-test-page.component.ts
+++ src/app/psyhology-test/pages/self-esteem-test-page/self-esteem-test-page.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {PopupDialogsService} from '../../../shared/popup-dialogs/popup-dialogs.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {testSelfEsteem} from './questions';
+import {answersTest, variantAnswer} from './answer';
+import {current} from 'codelyzer/util/syntaxKind';
 
 @Component({
   selector: 'ml-self-esteem-test-page',
@@ -6,7 +12,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./self-esteem-test-page.component.scss']
 })
 export class SelfEsteemTestPageComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  questions;
+  answersControl;
+  answers;
+  questionsControl;
+  equalTest = false;
+  selfEsteemTest: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private info: PopupDialogsService,
+              private cd: ChangeDetectorRef,
+              private router: Router) {}
+
+  ngOnInit() {
+    this.questions = testSelfEsteem;
+    this.answers = variantAnswer;
+    this.answersControl = Object.keys(variantAnswer);
+    this.questionsControl = Object.keys(testSelfEsteem);
+    this.initForm();
+  }
+
+  initForm() {
+    this.selfEsteemTest = this.fb.group({});
+    this.createFormControl();
+  }
+
+  createFormControl() {
+    this.questionsControl.forEach(key => {
+      this.selfEsteemTest.setControl(key, this.fb.control('', [Validators.required]));
+    });
+  }
+
+  clearForm() {
+    this.selfEsteemTest.reset();
+  }
+
+  equalResponse() {
+    this.equalTest = true;
+    const dialogMsg = this.getAnswer();
+    this.info
+      .open(dialogMsg, 'info')
+      .afterClosed()
+      .finally(() => {
+        this.equalTest = false;
+        this.cd.detectChanges();
+      })
+      .subscribe(rs => {
+        if (!rs) {
+          this.clearForm();
+        }
+        this.router.navigateByUrl(rs ? '/tests/list' : '/tests/self-esteem');
+      });
+  }
+
+  getSummer() {
+    const numberArray = Object.keys(this.selfEsteemTest.value).map(item => this.convertAnswerTonumber(this.selfEsteemTest.value[item]));
+
+    return  numberArray.reduce((sum, _current) => sum + _current, 0);
+  }
+
+  convertAnswerTonumber(answer) {
+    switch (answer) {
+      case 'a':
+        return 4;
+      case 'b':
+        return 3;
+      case 'c':
+        return 2;
+      case 'd':
+        return 1;
+      default:
+        return 0;
+    }
+  }
+
+  getAnswer() {
+    let message;
+    const answer = this.getSummer();
+
+    if (answer <= 25) {
+      message = answersTest['0'];
+    } else if (answer > 25 && answer <= 46) {
+      message = answersTest['26'];
+    } else {
+      message = answersTest['46'];
+    }
+
+    return message;
+  }
 }
