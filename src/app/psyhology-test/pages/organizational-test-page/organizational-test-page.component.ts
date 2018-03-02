18ffa4f3a1d0e1ea2b0bb280e7e82e0c4ae04198
Misha <tymish1@gamail.com>
2018-03-02 17:40:41 +0200

    added template and methods for equal result for answer test organization

--- src/app/psyhology-test/pages/organizational-test-page/organizational-test-page.component.ts
+++ src/app/psyhology-test/pages/organizational-test-page/organizational-test-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {PopupDialogsService} from '../../../shared/popup-dialogs/popup-dialogs.service';
+import {testOrganizator} from './questions';
+import {answersTest, variantAnswer} from './answer';
 
 @Component({
   selector: 'ml-organizational-test-page',
@@ -6,7 +11,105 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organizational-test-page.component.scss']
 })
 export class OrganizationalTestPageComponent implements OnInit {
-  constructor() {}
+  questions;
+  answersControl;
+  answers;
+  questionsControl;
+  equalTest = false;
+  organizatorTest: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+    private fb: FormBuilder,
+    private info: PopupDialogsService,
+    private cd: ChangeDetectorRef,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.questions = testOrganizator;
+    this.answers = variantAnswer;
+    this.answersControl = Object.keys(variantAnswer);
+    this.questionsControl = Object.keys(testOrganizator);
+    this.initForm();
+  }
+
+  initForm() {
+    this.organizatorTest = this.fb.group({});
+    this.createFormControl();
+  }
+
+  createFormControl() {
+    this.questionsControl.forEach(key => {
+      this.organizatorTest.setControl(key, this.fb.control('', [Validators.required]));
+    });
+  }
+
+  clearForm() {
+    this.organizatorTest.reset();
+  }
+
+  equalResponse() {
+    this.equalTest = true;
+    const dialogMsg = this.getAnswer();
+    this.info
+      .open(dialogMsg, 'info')
+      .afterClosed()
+      .finally(() => {
+        this.equalTest = false;
+        this.cd.detectChanges();
+      })
+      .subscribe(rs => {
+        if (!rs) {
+          this.clearForm();
+        }
+        this.router.navigateByUrl(rs ? '/tests/list' : '/tests/organizational');
+      });
+  }
+
+  getAnswer() {
+    let message;
+    const answer = this.getSummer();
+
+    if (answer < 40) {
+      message = answersTest['0'];
+    } else if (answer >= 40 && answer < 70) {
+      message = answersTest['40'];
+    } else {
+      message = answersTest['70'];
+    }
+
+    return message;
+  }
+
+  getSummer() {
+    const answerA = this.getNumberAnswer('A').filter(item => item === 'true').length;
+    const answerB = this.getNumberAnswer('B').filter(item => item === 'false').length;
+
+    return ((answerA + answerB) * 100) / 20;
+  }
+
+  getNumberAnswer(typeAnswer: 'A' | 'B') {
+    const numberAnswer = typeAnswer === 'A' ? [1, 3, 5, 7, 9, 11, 13, 17, 18, 19, 20] : [2, 4, 6, 8, 10, 12, 14, 15, 16];
+    const answerKey = Object.keys(this.organizatorTest.value).map(item => this.getElementArray(numberAnswer, item));
+
+    return answerKey.filter(item => item);
+  }
+
+  getElementArray(numberAnswer, item) {
+    if (numberAnswer.indexOf(this.getStringMatchNumber(item)) > -1) {
+      return this.convertAnswerTonumber(this.organizatorTest.value[item]);
+    }
+  }
+  getStringMatchNumber(item) {
+    return +item.match(/\d+/)[0];
+  }
+
+  convertAnswerTonumber(answer) {
+    switch (answer) {
+      case 'a':
+        return 'true';
+      default:
+        return 'false';
+    }
+  }
 }
