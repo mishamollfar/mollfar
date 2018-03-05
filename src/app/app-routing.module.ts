ffb9423c09e58f0ddecf2ad7e2f683f429df7f58
Misha <tymish1@gamail.com>
2018-03-05 14:14:50 +0200

    added contact page template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ export const routes: Routes = [
   { path: '', pathMatch: 'full', component: LandingPageComponent, data: { title: 'Главная' } },
   { path: 'home', component: HomePageComponent, data: { title: 'Головна' } },
   // { path: 'news', component: NewsPageComponent, data: { title: 'Новини' } },
-  // { path: 'contact', component: ContactPageComponent, data: { title: 'Контакти' } },
+  { path: 'contact', component: ContactPageComponent, data: { title: 'Контакти' } },
   // { path: 'photo', component: PhotoGalleryPageComponent, data: { title: 'Фотогалерея' } },
   { path: 'tests', loadChildren: 'app/psyhology-test/psyhology-test.module#PsyhologyTestModule' },
   { path: '**', redirectTo: '/' }
