44dad8ffd661d3ade51161d2f8d104c8f323e8d6
Misha <tymish1@gamail.com>
2018-03-05 19:06:33 +0200

    added new template and uncommitted rout news

--- src/app/pages/news-page/news-page.component.ts
+++ src/app/pages/news-page/news-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Feed, parseXmlToJson, RssFeedApiService} from '../../service/rss-feed-api.service';
+
 
 @Component({
   selector: 'ml-news-page',
@@ -6,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-page.component.scss']
 })
 export class NewsPageComponent implements OnInit {
-  constructor() {}
+  news: Feed[];
+  constructor(private feed: RssFeedApiService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const feedUrl2 = 'https://www.rbc.ua/static/rss/newsline.img.ukr.rss.xml';
+
+    this.feed.getFeeds(feedUrl2)
+      .subscribe(rs => {
+          this.news = parseXmlToJson(rs);
+        }, err => console.log('error', err)
+      );
+  }
 }
