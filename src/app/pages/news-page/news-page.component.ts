85e668a143cbdaef2756ed324ca05d67bea949da
Misha <tymish1@gamail.com>
2018-03-16 14:59:59 +0200

    replace template to tab component

--- src/app/pages/news-page/news-page.component.ts
+++ src/app/pages/news-page/news-page.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import { Feed, parseXmlToJson, RssFeedApiService } from '../../service/rss-feed-api.service';
 import { MatTabChangeEvent } from '@angular/material';
 
@@ -12,16 +12,17 @@ export class NewsPageComponent implements OnInit {
   indexTab = 0;
   news: Feed[];
   feed = {
-    0: 'https://blog.angular.io/feed',
+    0: 'https://www.rbc.ua/static/rss/newsline.img.ukr.rss.xml',
     1: 'https://codeguida.com/feeds/',
     2: 'https://habrahabr.ru/rss/interesting/',
     3: 'https://tokar.ua/feed',
-    4: 'https://www.rbc.ua/static/rss/newsline.img.ukr.rss.xml',
+    4: 'https://blog.angular.io/feed',
     5: 'https://stackoverflow.com/feeds',
-    6: 'http://k.img.com.ua/rss/ua/technews.xml'
+    6: 'http://k.img.com.ua/rss/ua/technews.xml',
+    7: 'https://espreso.tv/rss'
   };
 
-  constructor(private feedApi: RssFeedApiService) {}
+  constructor(private feedApi: RssFeedApiService, private cd: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.changeTab();
@@ -30,10 +31,10 @@ export class NewsPageComponent implements OnInit {
   changeTab(event?: MatTabChangeEvent) {
     delete this.news;
     this.indexTab = event ? event.index : this.indexTab;
-    this.feedApi.getFeeds(this.feed[this.indexTab]).subscribe(
-      rs => {
-        this.news = parseXmlToJson(rs);
-      },
+    this.feedApi.getFeeds(this.feed[this.indexTab])
+      .finally(() => this.cd.detectChanges())
+      .subscribe(
+      rs => this.news = parseXmlToJson(rs),
       err => console.log('error', err)
     );
   }
